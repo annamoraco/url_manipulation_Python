95021b3163f832777d1952c0a15b849157e08494
Anna Moraco <annamoraco@gmail.com>
2021-08-31 09:30:27 -0300

    Primeiro commit - entendendo url's

---
+++ main.py
@@ -0,0 +1,8 @@
+url = "https://www.linkedin.com/search/results/people/?keywords=engenheiro&origin=CLUSTER_EXPANSION&sid=d7k"
+print(url)
+
+url_base = url[0:47]
+print(url_base)
+
+url_parametros = url[47:len(url)]
+print(url_parametros)
